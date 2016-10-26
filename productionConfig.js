7a334dbd3704485350ce861257ffce4607fbc3b6
Numose <andrew.cernek@gmail.com>
2016-10-26 12:16:00 -0700

    resolve merge conflict

--- productionConfig.js
+++ productionConfig.js
@@ -1,4 +1,3 @@
-<<<<<<< 3e594682ba1eba23ff6ba8f77631b53cc51c182f
 var app = require('./server.js');
 var inquirer = require('inquirer');
 process.env.dev = false;
@@ -7,17 +6,6 @@ const mongoose = require('mongoose');
 
 const POSTINGSURI;
 mongoose.connect(POSTINGSURI);
-=======
-const app = require('./server.js');
-const inquirer = require('inquirer');
-const keysMethods = require('./services/keys.js');
-
-const POSTINGSURI = keysMethods.getConnectionString();
-
-const port = 8000;
-const mongoose = require('mongoose');
-
->>>>>>> remove public connection string
 console.log('URI:', POSTINGSURI);
 
 // inquirer.prompt({type: 'confirm', message: 'connect to remote db?'}).then(
@@ -25,6 +13,7 @@ console.log('URI:', POSTINGSURI);
 //     mongoose.connect(POSTINGSURI);
 //   }
 // );
+
 
 //------------------server listen------------------------------
 //-------------------------------------------------------------
