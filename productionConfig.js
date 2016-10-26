2aa51437cec6369e867571d922121c7194999b25
Numose <andrew.cernek@gmail.com>
2016-10-26 09:04:15 -0700

    remove public connection string

--- productionConfig.js
+++ productionConfig.js
@@ -1,3 +1,4 @@
+<<<<<<< 3e594682ba1eba23ff6ba8f77631b53cc51c182f
 var app = require('./server.js');
 var inquirer = require('inquirer');
 process.env.dev = false;
@@ -6,6 +7,17 @@ const mongoose = require('mongoose');
 
 const POSTINGSURI;
 mongoose.connect(POSTINGSURI);
+=======
+const app = require('./server.js');
+const inquirer = require('inquirer');
+const keysMethods = require('./services/keys.js');
+
+const POSTINGSURI = keysMethods.getConnectionString();
+
+const port = 8000;
+const mongoose = require('mongoose');
+
+>>>>>>> remove public connection string
 console.log('URI:', POSTINGSURI);
 
 // inquirer.prompt({type: 'confirm', message: 'connect to remote db?'}).then(
